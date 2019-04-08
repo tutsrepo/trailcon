f34a0dc541de3ff07c3e35a3ecaf11ab7c6074b0
Kunal Kharidia <kunalkharidia@gmail.com>
2019-04-08 11:58:57 -0400

    Added model for contest page

---
+++ application/models/Contest_model.php
@@ -0,0 +1,45 @@
+<?php
+class Contest_model extends CI_Model {
+
+        public function __construct()
+        {
+                $this->load->database();
+        }
+
+		public function get_contests($id = FALSE)
+		{
+		        if ($id === FALSE)
+        		{
+                		$query = $this->db->get('contests');
+                		return $query->result_array();
+        		}
+
+        		$query = $this->db->get_where('contests', array('id' => $id));
+        		return $query->row_array();
+		}
+
+		public function addContests()
+		{
+
+    		$data = array(
+        		'firstname' => $this->input->post('firstname'),
+        		'lastname' => $this->input->post('lastname'),
+        		'email' => $this->input->post('email')
+    		);
+
+    		return $this->db->insert('contests', $data);
+		}
+
+		public function updateContests($id=0)
+		{
+
+    		$data = array(
+        		'firstname' => $this->input->post('firstname'),
+        		'lastname' => $this->input->post('lastname'),
+        		'email' => $this->input->post('email')
+    		);
+
+			$this->db->where('id',$id);
+			$this->db->update('contests',$data);
+		}
+}
