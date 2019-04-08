0f8d23ebab2e77862217677e4e0bd3d5c8ede5f8
Kunal Kharidia <kunalkharidia@gmail.com>
2019-04-08 12:06:39 -0400

    Added templates for header and footer in codebase

---
+++ application/views/templates/header.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+	<link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
